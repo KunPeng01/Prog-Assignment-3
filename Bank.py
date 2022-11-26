178f7062d742cc2574e04415098bb663ae8a779c
KunPeng01 <ykphrfly@gmail.com>
2022-11-25 23:58:50 -0500

    2.0

--- Bank.py
+++ Bank.py
@@ -1,65 +1,175 @@
 
+# define class account and methond 
+class Account:
+    def __init__(self,accountNumber, accountHolderName, rateOfinterest, currentchecquingbalance, currentsavingbalance):
+        self.accountNumber = accountNumber
+        self.accountHolderName = accountHolderName
+        self.rateOfinterest = rateOfinterest
+        self.currentchecquingbalance = currentchecquingbalance
+        self.currentsavingbalance  = currentsavingbalance
+
+    def getAccountNumber(self):
+        return self.accountNumber
+
+    def getAccountHolderName(self):
+        return self.accountHolderName
+
+    def getRateOfinterest(self):
+        return self.rateOfinterest
+
+    def getCurrentChecquingBalance(self):
+        return self.currentchecquingbalance
+
+    def getCurrentSavingBalance(self):
+        return self.currentsavingbalance
+
+    def SavingDeposit(self,amount):
+        self.currentsavingbalance = self.currentsavingbalance + amount 
+        return self.currentsavingbalance
+
+    def SavingWithdraw(self,amount):
+        self.currentsavingbalance = self.currentsavingbalance - amount
+        return self.currentsavingbalance
+
+    def ChecquingDeposit(self,amount):
+        self.currentchecquingbalance = self.currentchecquingbalance + amount
+        return self.currentchecquingbalance
+
+    def ChecquingWithdraw(self, amount):
+        self.currentchecquingbalance = self.currentchecquingbalance - amount
+        return self.currentchecquingbalance
+
+# define savingAccount class
+class SavingAccount(Account):
+    
+    def __init__(self,accountNumber, accountHolderName, rateOfinterest, currentchecquingbalance, currentsavingbalance):
+        super().__init__(accountNumber, accountHolderName, rateOfinterest, currentchecquingbalance, currentsavingbalance)
+        self.smin = 5000
+
+        
+    def Withdraw(self, amount):
+        i = super().SavingWithdraw(amount)
+        if i < self.smin:
+            print("Yon can't withdraw too much money.")
+            # display the original balance
+            t = i + amount
+            print("The balance is "+ str(t))
+        else:
+            print("Your current balance is: "+ str(i))
+            
+       
+class ChequingAccount(Account):
+    def __init__(self,accountNumber, accountHolderName, rateOfinterest, currentchecquingbalance, currentsavingbalance):
+        super().__init__(accountNumber, accountHolderName, rateOfinterest, currentchecquingbalance, currentsavingbalance)
+        self.overdraft = 5000
+
+    def Withdraw(self,amount):
+        i = super().ChecquingWithdraw(amount)
+        if i > self.overdraft:
+            print("You can't withdraw too much money.")
+            t = i + amount
+            print("The balance is "+ str(t))
+        else:
+            print("Yon current balance is: "+ str(i))
+
+
+
+class Bank:
+    
+    user1 = [1111, "Leo", 5.3, 3000,8000]
+    user2 = [2222,"Helen", 5.3, 3000,8000]
+    user3 = [3333, "John", 5.3, 3000,8000]
+    user4 = [4444, "Lily", 5.3, 3000,8000]
+    user5 = [5555, "Howard", 5.3, 3000,8000]
+    numbersearch =[user1[0],user2[0],user3[0],user4[0],user5[0]]
+    namesearch = [user1[1],user2[1],user3[1],user4[1],user5[1]]
+    numbersearch1 = [user1,user2,user3,user4,user5]
+
+    def OpenAccount(self):
+        
+        
+        print(Bank.user1,"\n",Bank.user2,"\n",Bank.user3,"\n",Bank.user4,"\n",Bank.user5)
+        
+        
+
+    def SearchAccount(self):
+        print("Please Select Following Options: ")
+        choice = int(input("1.Enter Account \n2.Exit  "))
+        if choice == 1:
+            ii = int(input("Please input your Account Number: "))
+            p = -1
+            for i in Bank.numbersearch:
+                if ii == i:
+                        p = p+1
+                        return Bank.numbersearch1[p]
+                        
+        else:
+            print("Thank you for using our service.") 
+                
 
 class Program:
-    def showMainMenu(self):
-        # while loop to display the following options until the user to choose to exit the application
-        print("Please select your account：")
-        print("1. Open Account \n2. Select Account \n3. Exit")
-        i = int(input("Please Enter: "))
-        while (i != 3):
-            print("1. Open Account \n2. Select Account \n3. Exit")
-            i = int(input("Please Enter: "))
-            if (i == 1):
-                pass
-            elif (i == 2):
-                pass
-        print("Exit")
-        return
-    
-    # define showAccountMenu
+    def showMianMenu(self):
+        t = Bank()
+        print("Welcome To CIBC!")
+        print("Account Information is As following: ")
+        t.OpenAccount()
+        user = t.SearchAccount()
+        # initial Account Class
+        global bankuser,bankusers,bankuserc
+        bankuser = Account(user[0],user[1],user[2],user[3],user[4])
+        bankusers = SavingAccount(user[0],user[1],user[2],user[3],user[4])
+        bankuserc = ChequingAccount(user[0],user[1],user[2],user[3],user[4])
+
     def showAccountMenu(self):
-        print("Please select following options: ")
-        print("1. Check Balance \n2. Deposit \n3. Withdraw \n4.Exit Account")
-        i = int(input("Please enter: "))
-        while (i != 4):
-            if (i == 1):
-                pass
-            elif (i == 2):
-                pass
-            elif (i ==3):
-                pass
-        print("Exit Account")
-        t = Program()
-        t.showMainMenu()
-
-# define class bank, it will record accounts information and method search account, open account
-class Bank:
-    # create 5 accounts with hard coded values
-    def __init__(self):
-        self.user1 = ["Kevin", 1111, "aaaa", 6000]
-        self.user2 = ["James", 2222, "bbbb", 6000]
-        self.user3 = ["Leo", 3333, "cccc", 6000]
-        self.user4 = ["Linda", 4444, "dddd", 6000]
-        self.user5 = ["Helen", 5555, "eeee", 6000]
-        
-    
-    # Define the searchaccount method that accepts an account number as parameter
-    def searchAccount(self, number):
-        #
-            if number == self.user1[1]:
-                return self.user1
-            elif number == self.user2[1]:
-                return self.user2
-            elif number == self.user3[1]:
-                return self.user3
-            elif number == self.user4[1]:
-                return self.user4
-            elif number == self.user5[1]:
-                return self.user5
+        print("Please Select following Choice: ")
+        choice = int(input("1.Checquing Account\n2.Saving Account\n3.Exit  "))
+        if choice == 1:
+            print("Please Select following Choice: ")
+            checquingchoice = int(input("1.Check Balance\n2.Deposit\n3.Withdraw\n4.Exit Account "))
+            if checquingchoice == 1:
+                print("You current checquing balance is: "+str(bankuser.getCurrentChecquingBalance()))
+                t = Program()
+                t.showAccountMenu()
+            elif checquingchoice == 2:
+                depositamount = int(input("Please enter deposit amount: "))
+                bankuserc.ChecquingDeposit(depositamount)
+                print(bankuser.getCurrentChecquingBalance())
+                t = Program()
+                t.showAccountMenu()
+            elif checquingchoice == 3:
+                # call ChecquingAccount Method
+                withdrawamount = int(input("Please enter withdraw amount: "))
+                bankuserc.Withdraw(withdrawamount)
+                print(bankuserc.getCurrentChecquingBalance())
+                t = Program()
+                t.showAccountMenu()
             else:
-                return 
+                t = Program()
+                t.showMianMenu()
+        elif choice == 2:
+            print("Please Select following Choice: ")
+            savingchoice = int(input("1.Check Balance\n2.Deposit\n3.Withdraw\n4.Exit Account "))
+            if savingchoice == 1:
+                print(bankuser.getCurrentSavingBalance())
+                t = Program()
+                t.showAccountMenu()
+            elif savingchoice == 2:
+                depositamount = int(input("Please enter deposit amount: "))
+                bankuser.SavingDeposit(depositamount)
+                print(bankuser.getCurrentSavingBalance())
+                t = Program()
+                t.showAccountMenu()
+            elif savingchoice == 3:
+                withdrawamount = int(input("Please enter withdraw amount: "))
+                bankusers.Withdraw(withdrawamount)
+                print(bankuser.getCurrentSavingBalance())
+                t = Program()
+                t.showAccountMenu()
+            else:
+                t = Program()
+                t.showMianMenu()
+        else:
+            t = Program()
+            t.showMianMenu()
 
-#class Account:
-    #def SavingsAccount(self,a):
-        
-        
+
---
+++ Program.py
@@ -0,0 +1,8 @@
+import Bank
+def run():
+    T = Bank.Program()
+    T.showMianMenu()
+    T.showAccountMenu()
+
+run()    
+    
